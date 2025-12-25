8dc9772778acd577f66db3f9a19b363c03929ef1
mohammed gouse <gousemohammed163@gmail.com>
2025-12-25 10:19:42 +0530

    Add basic HTTP server using Node.js

---
+++ 04 - HTTP Server/index.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const log = `${Date.now}: ${req.url} New request recieved\n`
+    fs.appendFile('log.txt', log, (err, data) => {
+        switch(req.url) {
+            case "/":
+                res.end("Home Page")
+                break;
+            case "/about":
+                res.end("I am Mohammed Gouse")
+                break;
+            default:
+                res.end("404 Not Found")
+        }
+    })
+})
+
+server.listen(8000, () =>  console.log("server started"))
