8d572baf72cf273da585b691c94374937b8cf083
mohammed gouse <gousemohammed163@gmail.com>
2025-12-27 12:02:13 +0530

    fix date.now and add search case

--- 05 - Handling URL/index.js
+++ 05 - Handling URL/index.js
@@ -4,7 +4,7 @@ const url = require('url');
 
 const server = http.createServer((req, res) => {
     if(req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now}: ${req.url} New request recieved\n`;
+    const log = `${Date.now()}: ${req.url} New request recieved\n`;
     const myUrl = url.parse(req.url, true);
     fs.appendFile('log.txt', log, (err, data) => {
         switch(myUrl.pathname) {
@@ -15,6 +15,9 @@ const server = http.createServer((req, res) => {
                 const username = myUrl.query.myname;
                 res.end(`Hi ${username}`)
                 break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end(`Here are your results for ${search}`)
             default:
                 res.end("404 Not Found")
         }
