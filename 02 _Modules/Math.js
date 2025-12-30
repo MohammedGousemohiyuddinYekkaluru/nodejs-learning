059d9888b292b6563cc866611885ef185fe6dc5c
mohammed gouse <gousemohammed163@gmail.com>
2025-12-30 16:32:41 +0530

    update module.js by adding multiply and divide functions

--- 02 _Modules/Math.js
+++ 02 _Modules/Math.js
@@ -6,6 +6,15 @@ function sub(a, b){
     return a - b
 }
 
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    if (b === 0) throw console.error("can't divide by zero");
+    return a/b
+}
+
 module.exports = {
-    add, sub
+    add, sub, multiply, divide
 }
--- 02 _Modules/module.js
+++ 02 _Modules/module.js
@@ -1,4 +1,6 @@
-const {add, sub} = require("./Math")
+const {add, sub, multiply, divide} = require("./Math")
 
 console.log("The Math value is", add(2, 3));
 console.log("The Math value is", sub(20, 5));
+console.log("The Math value is", multiply(20, 5));
+console.log("The Math value is", divide(20, 5));
